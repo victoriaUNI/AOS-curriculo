a49af1124783fa09cc1e1d4c9ed1694456a58220
victoriaUNI <victoria.00000851663@unicap.br>
2025-04-21 14:10:13 -0300

    Add files via upload

---
+++ curriculo-api/src/main/java/com/seucurriculo/curriculo_api/CurriculoApiApplication.java
@@ -0,0 +1,13 @@
+package com.seucurriculo.curriculo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculoApiApplication.class, args);
+	}
+
+}
